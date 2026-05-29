4012ca641ecb22c2c88beac9be8b1ec8ee2a0fe2
lalit9050 <lalitlamba9050@gmail.com>
2026-05-29 15:35:09 +0530

    mail error

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
