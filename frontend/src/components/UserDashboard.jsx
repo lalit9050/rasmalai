3de385c2097e7441dccbf209ec3fe92b04d35f2c
lalit9050 <lalitlamba9050@gmail.com>
2026-05-30 16:15:40 +0530

    search functionality

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -77,7 +77,7 @@ function UserDashboard() {
       <Nav />
 
       {searchItems && searchItems.length > 0 && (
-        <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-5 bg-white shadow-md rounded-2xl mt-4'>
+        <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-5 bg-white shadow-md rounded-2xl mt-16 md:mt-4'>
           <h1 className='text-gray-900 text-2xl sm:text=3xl font-semibold border-b border-gray-200 pb-2'>Search Results</h1>
           <div className='w-full h-auto flex flex-wrap gap-6 justify-center'>
             {searchItems.map((item) => (
