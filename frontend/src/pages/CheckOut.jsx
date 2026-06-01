c7e30b468a8f7e3f88da343fcb24d80f7d90ff08
lalit9050 <lalitlamba9050@gmail.com>
2026-06-01 17:26:40 +0530

    marker error resolved

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -14,6 +14,18 @@ import { setAddress, setLocation } from '../redux/mapSlice';
 import axios from 'axios';
 import { addMyOrder } from '../redux/userSlice';
 
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+})
+
 function RecenterMap({ location }) {
     if (location.lat && location.lon) {
         const map = useMap()
